8030f483740295d6f150af71a60e55e163f8b0f0
munkhbold_max <151482159+munkhbold260@users.noreply.github.com>
2023-11-27 12:50:46 +0800

    Add files via upload

---
+++ js-basics/2.js
@@ -0,0 +1,49 @@
+// let myAge = 20;
+// let ageLimit = 21;
+// if (myAge >= ageLimit) {
+//   console.log("allowed to enter", myAge);
+// } else {
+//   console.log("not allowed to enter", myAge);
+// }
+
+// let myBirthYear = 1990;
+
+// console.log(2023 - myBirthYear);
+
+// let myNumber = 30;
+// console.log("myNumber=", myNumber);
+
+// // data types-> ugugdliin turul
+// let number1 = 15;   // ->number type
+// let myString = "15";   // -> spring type   (collection of character)
+// let myBool = false;  //boolean type
+
+// console.org("number1", number1);
+// console.org("myString", myString);
+
+let day = 6;
+if (day == 1) {
+  console.log("monday");
+} else if (day == 2) {
+  console.log("tuesday");
+} else if (day == 3) {
+  console.log("wednesday");
+} else if (day == 4) {
+  console.log("thursday");
+} else if (day == 5) {
+  console.log("friday");
+} else if (day == 6) {
+  console.log("saturday");
+} else if (day == 7) {
+  console.log("sunday");
+}
+
+// sariig oldog program. odor ogdono, ogogdson odoroos hamaar heddehi sard baigaag todorhoilno
+// jishee n 35 gevel 1r sard bish 2r sard baigaa bolhoor feburary
+// 65 gevel 3r sar buyu MARCH gesen text hevledeg
+// dung tootsoh program bichne.
+// 3n oor torliin dun taniulna (midterm1, midterm2, final)
+//hervee dundaj dun chine 60s doosh avsan bol dun chine F
+//hervee dundaj dun chine 60-70 hoorond bol avsan bol dun chine D
+//hervee dundaj dun chine 70-80 hoorond bol avsan bol dun chine C//...
+//EXTRA midterm- uud n dund 25% iar noloolno harin final dun buh dungiin  50% d noloolno.
---
+++ js-basics/first.js
@@ -0,0 +1,51 @@
+//let -> declare a variable -- huwisagch zarla
+// = -> assign a value -- utga uguh
+// assignment ->suulgaj uguh
+//; -> ( semi colon) muriin tugsguliig temdeglene
+
+let firstName = "boldoo";
+let myAge = 33;
+
+// consolole.log-> ur dung hwleh command
+
+console.log(firstName);
+console.log(myAge);
+
+let num1 = 1234;
+let num2 = 1162;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+let sum = num1 + num2;
+let product = num1 * num2;
+let division = num1 / num2;
+let subtraction = num1 - num2;
+
+// y = x*x + 2*x +1
+
+let x = 10;
+
+console.log(x * x + 2 * x + 1);
+
+//declare a variable called "number1" and "number2" and assign a value
+//and print out sum of the two variables
+
+let number = 15;
+number = number + 20;
+console.log(number);
+
+//syntax error- js uuriin gesen duremtei. tuuuniig n zurchuud bn.
+//reference error- hayg addressin aldaa
+
+// = -> right hand operator -assignmnet
+// + - * / -> left hand operator
+
+// data types-> ugugdliin turul
+let number1 = 15; // ->number type
+let myString = "15"; // -> spring type   (collection of character)
+
+console.org("number1", number1);
+console.org("myString", myString);
