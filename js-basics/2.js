a45a846cf2c22f62598ab9634af8eafa6af3aa91
bold_max <munkhbold260@gmail.com>
2023-11-28 22:41:12 +0800

    home workoo duugasan

--- js-basics/2.js
+++ js-basics/2.js
@@ -86,18 +86,19 @@ if (day < 32) {
 //console.log(((mid1 + mid2) / 2 + fin) / 2);
 let midterm1 = 70;
 let midterm2 = 70;
-let final = 70;
+let final = 100;
+let avg = ((midterm1 + midterm2) / 2 + final) / 2;
 
-if (((midterm1 + midterm2) / 2 + final) / 2 < 60) {
-  console.log("F", ((midterm1 + midterm2) / 2 + final) / 2);
-} else if (((midterm1 + midterm2) / 2 + final) / 2 < 70) {
-  console.log("D", ((midterm1 + midterm2) / 2 + final) / 2);
-} else if (((midterm1 + midterm2) / 2 + final) / 2 < 80) {
-  console.log("C", ((midterm1 + midterm2) / 2 + final) / 2);
-} else if (((midterm1 + midterm2) / 2 + final) / 2 < 90) {
-  console.log("B", ((midterm1 + midterm2) / 2 + final) / 2);
-} else if (((midterm1 + midterm2) / 2 + final) / 2 <= 100) {
-  console.log("A", ((midterm1 + midterm2) / 2 + final) / 2);
+if (avg < 60) {
+  console.log("F");
+} else if (avg < 70) {
+  console.log("D");
+} else if (avg < 80) {
+  console.log("C");
+} else if (avg < 90) {
+  console.log("B");
+} else if (avg <= 100) {
+  console.log("A");
 } else {
   console.log("other");
 }
--- js-basics/spoj/RGB7002.js
+++ js-basics/spoj/RGB7002.js
@@ -1,8 +1,5 @@
 function triPri(a, b, c) {
   return a + b + c;
 }
-
-// function calling
-
 let result = triPri(3, 4, 5);
 console.log(result);
