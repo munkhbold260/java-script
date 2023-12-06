af3855297c0bbb3dfadeeb2b4c3f2c5ee371d702
munkhbold260 <munkhbold260@gmail.com>
2023-12-06 13:09:02 +0800

    wdwd

--- js-basics/spoj/7100/RGB7104.js
+++ js-basics/spoj/7100/RGB7104.js
@@ -24,12 +24,7 @@
 // }
 // math(3, 2, 1, 4);
 
-<<<<<<< HEAD
-let math = [{ a: 8 }, { a: 9 }, { a: 3 }, { a: 4 }, { a: 7 }];
-=======
 let math = [{ a: 10 }, { a: 57 }, { a: 17 }, { a: 21 }, { a: 7 }];
->>>>>>> b68db1683b5ee2563d97ed0a6ebe96f839e223fb
-
 function findMostNumber() {
   let mostExp = math[0].a;
   for (let i = 1; i < math.length; i = i + 1) {
